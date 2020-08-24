ce067f9b925bc4f6615204df165b584011320a69
Sidharth Srinath <ssrinath@ucsd.edu>
2020-08-24 22:34:12 +0000

    working for sentences

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def generate(model, firstWords, sens):
         
 '''
 
-def generate(model,firstWord,numWords):
+def generateWords(model,firstWord,numWords):
     #model = dictionary
     #the key
     #numwords number of total words
@@ -68,9 +68,54 @@ def generate(model,firstWord,numWords):
         ind = random.randint(0,valsLength-1)
         word = vals[ind]
         #print(word)
-        return firstWord +' '+ generate(model,word,numWords-1)+' '
+        return firstWord +' '+ generateWords(model,word,numWords-1)+' '
+
+def generateSen(model, firstWords, result = ""):
+    #model = dictionary
+    #firstWords = the words that start each sentence, user input
+    #result = the final string that is a sum of all the words we add to it. The product we return
+    resultList = result.split() #list of words in result
+
+    
+    if len(resultList) == 0:
+        result += firstWords[0] + " "
+        firstWords.pop(0)
+        return generateSen(model,firstWords,result)
+
+        #vals = model[firstWords[0]] #list of possible next words depending on the first word (Case: first iteration)
+                
+    '''if len(firstWords) == -1: #base case, when we are out of sentence starters
+        result += '\n done \n'
+        return result #return the result because function is over
+
+    else:'''
+
+       
+    lastWord = resultList[len(resultList)-1]
+    vals = model[lastWord] #list of possible next words depending on the first word (Case: not first iteration)
+    valsLength = len(vals) # number of values we have to work with
+    
+    if valsLength > 1: #if multiple value possibilities, pick a random one
+        ind = random.randint(0, valsLength - 1)  # using random method to find a random index
+        word = vals[ind] #random word depending on random index
+        
+    elif valsLength == 1: # if there is only one word in vals
+        word = vals[0] #setting word to the only word in the list
+
+    if '.' in word:
+        result+= ". \nending sentence...\n"
+        try:
+            word = firstWords[0]
+            firstWords.pop(0)        
+        except:
+            result += '\n done \n'
+            return result
+    #print("added " + word)
+    result += (word + " ")  
+    return generateSen(model,firstWords,result)
+
 
 e = train(fileString)
-a = generate(e, "Barry" ,10)
+a = generateSen(e, ["Barry","Bee"])
 print(a)
         
